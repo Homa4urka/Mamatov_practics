d9c34ab3bc1189ad8a31cebcdc9bde9a7eee9c2e
Homa4urka <arsenymamatov@yandex.ru>
2025-10-15 21:20:37 +0300

    Delete 5.3.1.py

--- 5.3.1.py
+++
@@ -1,7 +0,0 @@
-def ramka(stroka):
-    dlina = int(len(stroka))
-    print('_' * (dlina + 4))
-    print('|', stroka, '|', sep=' ')
-    print('_' * (dlina + 4))
-stroka = str(input('Введите слово: '))
-ramka(stroka)
